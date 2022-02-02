a8f53455b28d45b8c2a64eeb2db77b129d92c6dc
Oliver Hauke <63044792+Olhaau@users.noreply.github.com>
2022-02-02 14:53:39 +0100

    Delete R directory

--- R/ml_measures.R
+++
@@ -1,7 +0,0 @@
-#' Calculates the Accuracy of a trained model
-#' @export
-acc = function(trained_model, test_dat, target, n_ref = NULL){
-  if(is.null(n_ref)){n_ref = nrow(test_dat)
-  } else {n_ref = round(as.numeric(n_ref))}
-  return(sum(predict(trained_model, test_dat[setdiff(names(test_dat), target)]) == test_dat[, target]) / n_ref)
-}
